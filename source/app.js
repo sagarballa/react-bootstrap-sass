a6972678ad051e91a215ec8bebce704bbc05157b
Sagar Balla <sagar.balla@gmail.com>
2018-01-28 21:42:27 +0530

    added animation

--- source/app.js
+++ source/app.js
@@ -3,14 +3,18 @@ import React from 'react';
 import Jumbotron from './bootstrap/jumbotron';
 import Button from './bootstrap/button';
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import './appsource.scss';
 
 export default class App extends React.Component {
     render(){
-        return <Jumbotron className="-fluid text-center custom">
+        return <ReactCSSTransitionGroup transitionName='app' transitionAppear={true} transitionAppearTimeout={500}>
+            <Jumbotron className="-fluid text-center custom">
             <h1>React with Bootstrap</h1>
             <p>with SASS</p>
             <Button className="-primary -lg" href="http://02geek.com" target="_blank">Click Here</Button>
-        </Jumbotron>;
+        </Jumbotron>
+        </ReactCSSTransitionGroup>;
     }
 }
